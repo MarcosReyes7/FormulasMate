cd9355cd4bb7e624590969d847bcbb42533f074c
MarcosReyes7 <111389234+MarcosReyes7@users.noreply.github.com>
2022-08-16 16:53:51 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -4,33 +4,18 @@ import time
 class bcolors:
     OK = '\033[92m' #GREEN
     WARNING = '\033[93m' #YELLOW
-    AZULC = '\033[90m'
+    AZULC = '\033[90m' #AZUL FONDO
     FAIL = '\033[91m' #RED
     RESET = '\033[0m' #RESET COLOR
     
 #============Inicio del juego=======================
 print(bcolors.AZULC+"Hola bienvenido a La leyenda del angel caído"+bcolors.RESET)
-print("Cargando...1%")
-time.sleep(2)
-print("Cargando...15%")
-time.sleep(2)
-print("Cargando...20%")
-time.sleep(2)
-print("Cargando...40%")
-time.sleep(2)
-print("Cargando...50%")
-time.sleep(2)
-print("Cargando...78%")
-time.sleep(2)
-print("Cargando...90%")
-time.sleep(2)
-print("Cargando...100%")
-time.sleep(2)
-print(" ")
+
+print(" ") #Sirve para dar espacios en blanco 
 print(" ")
     #=========Lobby=================================
 a = "MENU"
-print(a.center(20))
+print(a.center(20)) #sirve para poner el texto en el lugar que necesites
 print(" ")
 time.sleep(5)
 print(bcolors.OK+"Selecciona [si] o [no]"+bcolors.RESET)
@@ -52,14 +37,17 @@ if star=="si":
 if p1=="si":
     print(" ")
     print("Entonces por que")
-else:
+elif p1=="no":
     print(" ")
     print("Estas mal de la cabeza")
+if p1=="no":
+    print("FIN")
 #==================Segunda parte=====================
-if p1=="si":
+if p1=="si" and p1=="no":  #Modificar
     print(" ")
     print(bcolors.OK+"Selecciona [tu] o [yo]"+bcolors.RESET)
-    p2=str(input("Quien te hizo tanto daño:"))
+p2=str(input("Quien te hizo tanto daño:"))
+
     
 if p2=="tu":
     print(" ")
